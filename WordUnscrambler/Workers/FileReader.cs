efa7c22f0747f0f4c6846020b6ae67db703ccfbf
Mooose17 <tom@moose17.dev>
2023-07-17 14:28:29 +0100

    create a generic file reader based off fileName

---
+++ WordUnscrambler/Workers/FileReader.cs
@@ -0,0 +1,20 @@
+
+namespace WordUnscrambler.Workers;
+
+public class FileReader
+{
+    public string[] Read(string fileName)
+    {
+        string[] fileContent;
+        try
+        {
+            fileContent = File.ReadAllLines(fileName);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+
+        return fileContent;
+    }
+}
