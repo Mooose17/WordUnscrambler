756412763374fca1b5a1c2653b987cd3de5b0321
Mooose17 <tom@moose17.dev>
2023-07-18 09:47:59 +0100

    adds unit tests for WordMatcher method

--- WordUnscrambler.Test.Unit/WordMatcherTest.cs
+++ WordUnscrambler.Test.Unit/WordMatcherTest.cs
@@ -24,4 +24,20 @@ public class WordMatcherTest
         Assert.IsTrue(matchedWords[0].ScrambledWord.Equals("omre"));
         Assert.IsTrue(matchedWords[0].Word.Equals("more"));
     }
+    [Test]
+    public void ScrambledWordMatchesGivenWordsInTheList()
+    {
+        string[] words = { "cat", "chair", "more" };
+        string[] scrambledWords = { "omre", "act" };
+
+        var matchedWords = _wordMatcher.Match(scrambledWords, words);
+        
+        Console.WriteLine(matchedWords);
+        
+        Assert.IsTrue(matchedWords.Count == 2);
+        Assert.IsTrue(matchedWords[0].ScrambledWord.Equals("omre"));
+        Assert.IsTrue(matchedWords[1].ScrambledWord.Equals("act"));
+        Assert.IsTrue(matchedWords[0].Word.Equals("more"));
+        Assert.IsTrue(matchedWords[1].Word.Equals("cat"));
+    }
 }
